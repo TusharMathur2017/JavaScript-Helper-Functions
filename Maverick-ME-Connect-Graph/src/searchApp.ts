f74f84b33e329063adfbf937327329572c1fb877
tusharmathur2017 <tushar_mathur@live.com>
2023-12-19 13:57:29 +0530

    .

--- Maverick-ME-Connect-Graph/src/searchApp.ts
+++ Maverick-ME-Connect-Graph/src/searchApp.ts
@@ -40,13 +40,22 @@ export class SearchApp extends TeamsActivityHandler {
     );
     const authProvider = new TokenCredentialAuthenticationProvider(credential, { scopes: ["https://graph.microsoft.com/.default"] });
     const graphClient = Client.initWithMiddleware({ authProvider: authProvider });
-    
 
+    'Least@42tcm.onmicrosoft.com'; 'tushar_mathur@42tcm.onmicrosoft.com',"2023-12-20T09:00:00"
     let startTime = query.parameters[0].value;
     let endTime = query.parameters[1].value;
     let timeInterval = query.parameters[3].value;
     let attendeesString = query.parameters[4].value;
     let attendeesArray = attendeesString.Split(";");
+
+    console.clear();
+    console.log("**************");
+    console.log("");
+
+    let timestamps = divideTimeRange("2023-12-20T09:00:00", "2023-12-20T11:00:00", 30)
+    console.log(timestamps.join(", "));
+    console.log("");
+
     let scheduleInformation = {
       schedules: attendeesArray, //['Least@42tcm.onmicrosoft.com', 'tushar_mathur@42tcm.onmicrosoft.com'],
       startTime: {
@@ -59,51 +68,13 @@ export class SearchApp extends TeamsActivityHandler {
       },
       availabilityViewInterval: timeInterval
     };
-    console.clear();
-    console.log("**************");
-    console.log("**************");
+    console.log(scheduleInformation);
+    console.log("");
 
     const response = await graphClient.api('/users/calendar/getSchedule')
       .post(scheduleInformation);
     console.log(response);
-
-
-
-    // function divideTimeRange(timeRange) {
-    //   // Parse the start and end times from the input string
-    //   const parts = timeRange.split(" end: ");
-    //   const startTime = new Date(parts[0].split("start: ")[1]);
-    //   const endTime = new Date(parts[1]);
-    
-    //   // Create an empty array to store the timestamps
-    //   const timestamps = [];
-    
-    //   // Loop through the time range, adding 30-minute intervals until we reach the end time
-    //   while (startTime <= endTime) {
-    //     timestamps.push(startTime.toISOString());
-    //     startTime.addMinutes(30);
-    //   }
-    
-    //   return timestamps;
-    // }
-    
-    // // Example usage
-    // const timeRange = "start: 2023-12-19T09:00:00 end: 2023-12-19T10:30:00";
-    // const timestamps = divideTimeRange(timeRange);
-    
-    // console.log("Time range:");
-    // console.log(timeRange);
-    // console.log("30-minute slots:");
-    // console.log(timestamps.join(", "));
-    
-
-
-
-
-
-
-
-
+    console.log("");
 
     const attachments = [];
     response.data.objects.forEach((obj) => {
@@ -129,40 +100,52 @@ export class SearchApp extends TeamsActivityHandler {
   }
 }
 
+function divideTimeRange(startTime, endTime, duration) {
+  let stTime = new Date(startTime);
+  let enTime = new Date(endTime);
+  const timestamps = [];
+  while (stTime <= enTime) {
+    timestamps.push(stTime);
+    let nextSlot = new Date(stTime.getTime() + duration * 60000);
+    stTime = nextSlot;
+  }
+  return timestamps;
+}
+
 // const client = Client.initWithMiddleware({
-    // 	debugLogging: true,
-    // 	authProvider,
-    // });
-    // const res = await graphClient.api("/users/").get();
-    // const meetingTimeSuggestionsResult = {
-    //   attendees: [
-    //     {
-    //       type: 'required',
-    //       emailAddress: {
-    //         name: 'Alen',
-    //         address: 'Alen@42tcm.onmicrosoft.com'
-    //       }
-    //     }
-    //   ],
-    //   timeConstraint: {
-    //     activityDomain: 'work',
-    //     timeSlots: [
-    //       {
-    //         start: {
-    //           dateTime: '2023-12-19T09:00:00',
-    //           timeZone: 'India Standard Time'
-    //         },
-    //         end: {
-    //           dateTime: '2023-12-19T17:00:00',
-    //           timeZone: 'India Standard Time'
-    //         }
-    //       }
-    //     ]
-    //   },
-    //   isOrganizerOptional: 'false',
-    //   meetingDuration: 'PT1H',
-    //   returnSuggestionReasons: 'true',
-    //   minimumAttendeePercentage: 100
-    // };
-    // const res = await graphClient.api('/users/Alen@42tcm.onmicrosoft.com/findMeetingTimes')
-    //   .post(meetingTimeSuggestionsResult)
+// 	debugLogging: true,
+// 	authProvider,
+// });
+// const res = await graphClient.api("/users/").get();
+// const meetingTimeSuggestionsResult = {
+//   attendees: [
+//     {
+//       type: 'required',
+//       emailAddress: {
+//         name: 'Alen',
+//         address: 'Alen@42tcm.onmicrosoft.com'
+//       }
+//     }
+//   ],
+//   timeConstraint: {
+//     activityDomain: 'work',
+//     timeSlots: [
+//       {
+//         start: {
+//           dateTime: '2023-12-19T09:00:00',
+//           timeZone: 'India Standard Time'
+//         },
+//         end: {
+//           dateTime: '2023-12-19T17:00:00',
+//           timeZone: 'India Standard Time'
+//         }
+//       }
+//     ]
+//   },
+//   isOrganizerOptional: 'false',
+//   meetingDuration: 'PT1H',
+//   returnSuggestionReasons: 'true',
+//   minimumAttendeePercentage: 100
+// };
+// const res = await graphClient.api('/users/Alen@42tcm.onmicrosoft.com/findMeetingTimes')
+//   .post(meetingTimeSuggestionsResult)
